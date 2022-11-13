2b2c8cf24572f007e85076f41b7239da82366caa
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-11-13 13:52:38 -0300

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  int n,s,a;
+    printf("digite um numero");
+    scanf("%d",&n);
+   a=n-1;
+   s=n+1;
+printf("o antecessor:%d \n",a);
+printf("o sucessor:%d \n",s);
+return 0;
+}
